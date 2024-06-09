a4dbf29f9485f73bd3b8801a55dd0e08f8a688a3
Sergey Khoperskov <sergey.khoperskov@gmail.com>
2024-06-10 00:22:31 +0200

    some changes

--- src/cell_convert_part.c
+++ src/cell_convert_part.c
@@ -1197,6 +1197,7 @@ struct spart *cell_spawn_new_spart_from_spart(struct engine *e, struct cell *c,
 
   /* Assign the ID. */
   sp_new->id = space_get_new_unique_id(e->s);
+  message("sp id done");
   gp->type = swift_type_stars;
   message("IDs done");
 
