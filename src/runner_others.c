06619fac6bf0562c5384aa47577497b4df044451
Sergey Khoperskov <sergey.khoperskov@gmail.com>
2024-06-10 13:07:05 +0200

    some changes

--- src/runner_others.c
+++ src/runner_others.c
@@ -541,18 +541,18 @@ if(current_stars_count != c->stars.count)
     {
       /* Get a handle on the part. */    
 //      struct spart *restrict spp = &sparts[k];
-      struct spart *restrict spp = &sparts[k];
+      struct spart * spp = &sparts[k];
       if (abs(spp->birth_time-e->time)<1e-8)
       {      
 
         message("a spart-old count %d",c->stars.count);
         message("b spart id %d %lld %e",k,spp->id,spp->mass);
 
-        struct spart *restrict sp_new = cell_add_spart(e, c);
+        struct spart * sp_new = cell_add_spart(e, c);
         sp_new->id = space_get_new_unique_id(e->s);
         message("c spart-new id %d %lld %lld %e",k,spp->id,sp_new->id,spp->mass);
         message("d spart-new count %d",c->stars.count);
-        struct gpart *gp = cell_add_gpart(e, c);
+        struct gpart * gp = cell_add_gpart(e, c);
         sp_new->gpart = gp;
         message("e spart-new id %d %lld %lld %e",k,spp->id,sp_new->id,spp->mass);
         message("f spart-new count %d",c->stars.count);
